cdeb3fbdbe122200f2f4599898b3070f561abe95
Vinicius <contato@viniccius.com.br>
2025-01-23 11:46:35 -0300

    feat: desable electron

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,22 @@ import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 import { VitePWA } from 'vite-plugin-pwa'
-import electron from 'vite-plugin-electron/simple'
+
+// import electron from 'vite-plugin-electron/simple'
+
+// electron({
+//   main: {
+//     entry: 'electron/main.ts',
+//   },
+//   preload: {
+//     input: 'electron/preload.ts',
+//   },
+//   renderer: {},
+// })
 
 export default defineConfig({
   plugins: [react(),
-    electron({
-      main: {
-        entry: 'electron/main.ts',
-      },
-      preload: {
-        input: 'electron/preload.ts',
-      },
-      renderer: {},
-    }),
+    
     VitePWA({
     registerType: 'autoUpdate',
     manifest: {
