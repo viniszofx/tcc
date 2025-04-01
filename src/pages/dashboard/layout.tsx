5891e7e70ab5b4e9992f01b9f5c97d2416a45ebe
Pedro <pedroesnarriaga@gmail.com>
2025-04-01 12:18:34 -0300

    feature: header dashoboard page

--- src/pages/dashboard/layout.tsx
+++ src/pages/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { AppSidebar } from "@/components/custom/app-sidebar";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-separator";
 import React from "react";
@@ -13,15 +13,14 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
                     <header className="flex h-16 shrink-0 items-center md:gap-12 border-b px-4">
                         <SidebarTrigger className="-ml-1" />
                         <Separator orientation="vertical" className="mr-2 h-4" />
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem>
-                                    <BreadcrumbLink className="text-lg font-bold" href="/dashboard">
-                                        Processamento
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
+                        <div>
+                            <p className="md:text-lg font-bold text-[var(--font-color)]">Processamento</p>
+                        </div>
+                        <div>
+                            <Button className="bg-[var(--secondary-color)]" asChild>
+                                <a href="/dashboard">Abrir Comissão</a>
+                            </Button>
+                        </div>
                     </header>
                     <main className="flex flex-col w-full bg-[var(--card-color)] flex-1">
                         {children}
