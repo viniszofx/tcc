50b2b91884e3a621c92413098708df913367aabe
Pedro <pedroesnarriaga@gmail.com>
2025-04-01 11:46:11 -0300

    refactor(dashboard): simplify layout and sidebar components
    Remove unnecessary breadcrumb items and streamline the sidebar by replacing complex navigation with a simplified menu. This improves maintainability and reduces clutter in the UI.

--- src/components/custom/app-sidebar.tsx
+++ src/components/custom/app-sidebar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
+import { Home, Settings } from "lucide-react"
+import { Button } from "../ui/button"
 
-import { SearchForm } from "@/components/custom/search-form"
-import { VersionSwitcher } from "@/components/custom/version-switcher"
 import {
   Sidebar,
   SidebarContent,
@@ -10,171 +10,37 @@ import {
   SidebarGroupLabel,
   SidebarHeader,
   SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
   SidebarRail,
 } from "@/components/ui/sidebar"
-
-// This is sample data.
-const data = {
-  versions: ["1.0.1", "1.1.0-alpha", "2.0.0-beta1"],
-  navMain: [
-    {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Building Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
-        },
-      ],
-    },
-  ],
-}
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
     <Sidebar {...props}>
-      <SidebarHeader>
-        <VersionSwitcher
-          versions={data.versions}
-          defaultVersion={data.versions[0]}
-        />
-        <SearchForm />
+      <SidebarHeader className="bg-[var(--secondary-color)]">
+        <div>
+          <img className="mx-auto h-auto" src="/Logotipo.svg" alt="logo" />
+        </div>
       </SidebarHeader>
-      <SidebarContent>
-        {/* We create a SidebarGroup for each parent. */}
-        {data.navMain.map((item) => (
-          <SidebarGroup key={item.title}>
-            <SidebarGroupLabel>{item.title}</SidebarGroupLabel>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                {item.items.map((item) => (
-                  <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
-                      <a href={item.url}>{item.title}</a>
-                    </SidebarMenuButton>
-                  </SidebarMenuItem>
-                ))}
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        ))}
+      <SidebarContent className="bg-[var(--secondary-color)] flex flex-col justify-between h-full">
+        <SidebarGroup>
+          <SidebarGroupLabel></SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              <li className="p-2 gap-2 text-[var(--font-color2)] flex items-center justify-center">
+                <Home />
+                <Button className="bg-[var(--secondary-color)]" asChild>
+                  <a href="/dashboard">Dashboard</a>
+                </Button>
+              </li>
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <div className="p-2 gap-2 text-[var(--font-color2)] flex items-center justify-center mb-12">
+          <Settings />
+          <Button className="bg-[var(--secondary-color)]" asChild>
+            <a href="/settings">Configurações</a>
+          </Button>
+        </div>
       </SidebarContent>
       <SidebarRail />
     </Sidebar>
--- src/pages/dashboard/layout.tsx
+++ src/pages/dashboard/layout.tsx
@@ -1,5 +1,5 @@
 import { AppSidebar } from "@/components/custom/app-sidebar";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbPage, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "@/components/ui/breadcrumb";
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { Separator } from "@radix-ui/react-separator";
 import React from "react";
@@ -10,19 +10,15 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
             <SidebarProvider>
                 <AppSidebar />
                 <SidebarInset>
-                    <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+                    <header className="flex h-16 shrink-0 items-center md:gap-12 border-b px-4">
                         <SidebarTrigger className="-ml-1" />
                         <Separator orientation="vertical" className="mr-2 h-4" />
                         <Breadcrumb>
                             <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="#">
-                                        Building Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block" />
                                 <BreadcrumbItem>
-                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                                    <BreadcrumbLink className="text-lg font-bold" href="/dashboard">
+                                        Processamento
+                                    </BreadcrumbLink>
                                 </BreadcrumbItem>
                             </BreadcrumbList>
                         </Breadcrumb>
@@ -32,7 +28,6 @@ export function DashboardLayout({ children }: { children: React.ReactNode }) {
                     </main>
                 </SidebarInset>
             </SidebarProvider>
-
         </div>
     );
 }
