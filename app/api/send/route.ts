bee812c71213fcf5c144a71128cd6e6d32692ba3
Vinicius Souza <contato@viniccius.com.br>
2025-04-09 17:49:07 -0300

    fix: resend

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,15 +1,15 @@
-import { EmailTemplate } from '@/components/e-mail';
-import { Resend } from 'resend';
+import { EmailTemplate } from "@/components/e-mail";
+import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || "");
 
 export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'teste@tcc.viniccius.com.br',
-      to: ['pedroesnarriaga@gmail.com'],
-      subject: 'Hello world',
-      react: await EmailTemplate({ firstName: 'Vinicius' }),
+      from: "teste@tcc.viniccius.com.br",
+      to: ["pedroesnarriaga@gmail.com"],
+      subject: "Hello world",
+      react: await EmailTemplate({ firstName: "Vinicius" }),
     });
 
     if (error) {
