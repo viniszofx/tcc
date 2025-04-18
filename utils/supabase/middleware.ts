621ca02eb0e6f8c678a2e4d93800c4860dc12b34
Vinicius Souza <contato@viniccius.com.br>
2025-04-18 00:40:27 -0300

    feat: improve logging in updateSession function for better debugging

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -18,6 +18,8 @@ export async function updateSession(request: NextRequest) {
   console.log("Supabase URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
   console.log("Supabase ANON KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
 
+  console.log("Request URL:", request.url);
+  console.log("Creating Supabase client with cookies");
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL! || "",
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY! || "",
@@ -40,24 +42,33 @@ export async function updateSession(request: NextRequest) {
       },
     }
   );
+  console.log("Supabase client created");
   // IMPORTANT: DO NOT REMOVE auth.getUser()
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
+  console.log("User:", user);
   const RoutesAllowed = [
     "/",
     "/auth/sign-in",
     "/api/v1/auth/callback",
     "/api/v1/auth/confirm",
   ];
-
+  console.log("RoutesAllowed:", RoutesAllowed);
   if (!user && !RoutesAllowed.includes(request.nextUrl.pathname)) {
+    console.log(
+      "User not found, redirecting to sign-in page",
+      request.nextUrl.pathname
+    );
     const url = request.nextUrl.clone();
+    console.log("Redirect URL:", url);
     url.pathname = "/auth/sign-in";
     url.searchParams.set("redirectedFrom", request.nextUrl.pathname);
     return NextResponse.redirect(url);
   }
-
+  console.log(
+    "User found, continuing to the requested page",
+    request.nextUrl.pathname
+  );
   return supabaseResponse;
 }
