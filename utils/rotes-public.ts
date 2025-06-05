06244357af93c46e2c04fcb35d2a44578d206012
Vinicius Souza <contato@viniccius.com.br>
2025-06-05 08:31:04 -0400

    feat(routes): add new API routes to publicRoutes for authentication and setup

--- utils/rotes-public.ts
+++ utils/rotes-public.ts
@@ -4,5 +4,9 @@ export const publicRoutes = [
   "/register",
   "/setup",
   "/recover-password",
-  "/demo/*",
+  "/api/v1/setup/",
+  "/api/v1/setup/status",
+  "/api/v1/auth/confirm",
+  "/api/v1/auth/callback",
+  "/api/v1/auth/redirect",
 ];
