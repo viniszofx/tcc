765ba456ad9a9b6c6bceb5223e4bbb2fa77e76fc
Vinicius Souza <contato@viniccius.com.br>
2025-06-17 20:39:27 -0400

    fix: remove inventory items from database and clean up data service

--- lib/data-service.ts
+++ lib/data-service.ts
@@ -1,30 +1,32 @@
-import data from "@/data/db.json"
+import data from "@/data/db.json";
 
 export const getOrganizations = () => {
-  return data.organizations
-}
+  return data.organizations;
+};
 
 export const getCampuses = () => {
-  return data.campuses
-}
+  return data.campuses;
+};
 
 export const getCommissions = (campusId?: string) => {
   if (campusId) {
-    return data.comissoes.filter((commission) => commission.campus_id === campusId)
+    return data.comissoes.filter(
+      (commission) => commission.campus_id === campusId
+    );
   }
-  return data.comissoes
-}
+  return data.comissoes;
+};
 
 export const getUsers = (campusId?: string) => {
   if (campusId) {
-    return data.users.filter((user) => user.campus_id === campusId)
+    return data.users.filter((user) => user.campus_id === campusId);
   }
-  return data.users
-}
+  return data.users;
+};
 
-export const getInventoryItems = (commissionId?: string) => {
-  if (commissionId) {
-    return data.inventory_items.filter((item) => item.comissao_id === commissionId)
-  }
-  return data.inventory_items
-}
+// export const getInventoryItems = (commissionId?: string) => {
+//   if (commissionId) {
+//     return data.inventory_items.filter((item) => item.comissao_id === commissionId)
+//   }
+//   return data.inventory_items
+// }
