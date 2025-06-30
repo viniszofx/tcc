f2152bfaa44265ba843329b1482a9bac08a70b37
Vinicius Souza <contato@viniccius.com.br>
2025-06-30 05:20:01 -0400

    feat: remove unused test routes and login page components

--- app/test-login/page.tsx
+++
@@ -1,9 +0,0 @@
-import SimpleAuthForm from "@/components/auth/simple-auth-form";
-
-export default function TestLoginPage() {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50">
-      <SimpleAuthForm />
-    </div>
-  );
-}
